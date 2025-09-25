0fba369d83f09831e5e3c8b7c6924eceff2c0930
ijvnoveoi <diegohan1008@gmail.com>
2025-09-25 17:19:15 +0800

    week03

---
+++ week03/week03-2a.cpp
@@ -0,0 +1,13 @@
+///week03-2a.cpp 璶糶ㄢΩ
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans = 1;///Τ1或跑或
+        for(int i=0; i<nums.size(); i++){///Τ碭计癹伴禲碭Ω
+            ans *= num[i];///–Ωрnums[i]秈ans柑
+        }///计禫禫1000计柑┮祘Α糶岿
+        if(ans>0) return 1;
+        if(ans<0) return -1;
+        return 0;
+    }
+};
